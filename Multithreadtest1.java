1defd48e6cd2aa3ec54780ac59face8ccf803d7b
192211967 <124856771+192211967@users.noreply.github.com>
2024-02-17 11:38:02 +0530

    Add files via upload

--- Multithreadtest1.java
+++ Multithreadtest1.java
@@ -3,21 +3,41 @@ class athread implements Runnable
 {
 	public void run()
 	{
+  try
+{
 		for(int i = 1; i<=5; i++) 
 		{
 			System.out.println(i+" X 7 = "+i*7);
-		}		
-	}
+Thread.sleep(1000);
+		}
+}		
+catch(Exception e)
+{
+System.out.println("e");
+}
+System.out.println("EXTENDS THREAD A");
+
+}
 }
 class bthread implements Runnable
 {
 	public void run()
+{
+try
 	{
 		for(int i = 1; i<=5; i++) 
 		{
 			System.out.println(i+" X 9 = "+i*9);
+Thread.sleep(2000);
 		}
-	}
+	
+}
+catch(Exception e)
+{
+System.out.println("e");
+}
+System.out.println("EXTENDS THREAD B");
+}
 }
 class Multithreadtest1
 {
@@ -28,6 +48,6 @@ class Multithreadtest1
 		bthread b = new bthread();
 		Thread t2=new Thread(b);
 		t1.start();
-t2.start();		
+                t2.start();		
 	}
 }
