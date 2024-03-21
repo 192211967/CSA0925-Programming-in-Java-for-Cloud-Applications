d7d96c1cc14c97e2bd346ad0027cc5299095391c
192211967 <124856771+192211967@users.noreply.github.com>
2024-03-21 16:09:56 +0530

    Add files via upload

--- CharacterSearch.java
+++ CharacterSearch.java
@@ -3,27 +3,28 @@ class CharacterSearch
 {
 	public static void main(String[] args) 
 	{
-        	Scanner sc = new Scanner(System.in);
+       	Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a string: ");
-        	String inputString = sc.nextLine();
+        	String N = sc.nextLine();
+			 int n=N.length();
 	        System.out.print("Enter a character to search: ");
         	char searchChar = sc.next().charAt(0);
         	int index = -1; 
-        	for (int i = 0; i < inputString.length(); i++) 
+        	for (int i = 0; i < n; i++) 
 		{
-            		if (inputString.charAt(i) == searchChar) 
-			{
+            		if (N.charAt(i) == searchChar) 
+			        {
                 		index = i; 
                 		break; 
             		}
-        	}
-                if (index != -1)
+       }
+       if (index != -1)
 		{
-            		System.out.println("The character '" + searchChar + "' is present at index " + index);
-        	} 
+           System.out.println("The character '" + searchChar + "' is present at index " + index);
+        } 
 		else 
 		{
-            		System.out.println("The character '" + searchChar + "' is not present in the string.");
-        	}
-       }
+ System.out.println("The character '" + searchChar + "' is not present in the string.");
+        }
+}
 }
